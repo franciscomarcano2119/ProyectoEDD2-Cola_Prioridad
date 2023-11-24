161ff18970d51a62096fe67aece95e21cdaac7db
AlejandroMorabito <131191251+AlejandroMorabito@users.noreply.github.com>
2023-11-24 17:17:51 -0400

    .

---
+++ src/proyectoedd_2_colaprioridad/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoedd_2_colaprioridad;
+
+import InterfacesGraficas.Bienvenida;
+
+/**
+ *
+ * @author yaxim
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bienvenida window = new Bienvenida();
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+    }
+    
+}
